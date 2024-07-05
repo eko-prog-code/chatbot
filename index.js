bc28d1b04758685619e7cdcab623ae516f0b94ea
yarnrn0 <yarnrn0@gmail.com>
2024-07-05 20:57:39 +0000

    oke

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ async function generateResponse(incomingMessages) {
   }
 }
 
+async function sendMessageAtSpecificTime(sock, number, message, targetDate) {
+  const currentTime = new Date().getTime();
+  const targetTime = targetDate.getTime();
+  const delay = targetTime - currentTime;
+
+  if (delay > 0) {
+    setTimeout(async () => {
+      try {
+        await sock.sendMessage(number, { text: message });
+        console.log(`Pesan terkirim ke ${number} pada ${targetDate}.`);
+      } catch (error) {
+        console.error(`Gagal mengirim pesan ke ${number}:`, error);
+      }
+    }, delay);
+  } else {
+    console.log('Waktu yang ditentukan sudah lewat.');
+  }
+}
+
 async function connectToWhatsApp() {
   try {
     console.log('Mencoba menghubungkan ke WhatsApp...');
@@ -55,9 +74,55 @@ async function connectToWhatsApp() {
         }
       } else if (connection === 'open') {
         console.log('Koneksi tersambung!');
+
+        // Tentukan tanggal dan waktu pengiriman untuk pesan pertama
+        const targetDate1 = new Date('2024-07-06T01:47:00+07:00'); // Waktu Indonesia, Jakarta
+
+        // Pesan dengan format yang diminta
+        const message1 = `
+          MedicTech :
+          🖥️ Icon Laptop RME 
+          💊 icon obat DosisAkurat.vercel.app
+          
+          Ini adalah pesan otomatis pada tanggal 6 Juli 2024 jam 1:47 AM.
+        `;
+
+        // Kirim pesan otomatis pada waktu yang ditentukan
+        sendMessageAtSpecificTime(sock, '6289633422255@s.whatsapp.net', message1, targetDate1);
+
+        // Tentukan tanggal dan waktu pengiriman untuk pesan kedua ke grup
+        const targetDate2 = new Date('2024-07-06T06:06:00+07:00');     // Waktu Indonesia, Jakarta
+
+        // Pesan untuk grup dengan format yang diminta
+        const message2 = `
+        Tetap semangat! ☕😄 Ingat, meskipun hari ini terasa panjang, 
+        kopi selalu lebih pendek daripada shift kita!
+          
+          Ini adalah pesan otomatis ke grup pada tanggal 6 Juli 2024 jam 06:06:06 AM.
+        `;
+
+        // Kirim pesan otomatis ke grup pada waktu yang ditentukan
+        sendMessageAtSpecificTime(sock, '120363044573094419@g.us', message2, targetDate2);
       }
     });
 
+    //info sock HMBI: '120363163312129637@g.us'
+    //info ppni Karawang '120363044573094419@g.us'
+    //ICO 1 '120363162183976678@g.us'
+    //ICO 2 '120363162768337998@g.us'
+    //Inno 1 '120363029562607086@g.us'
+    //Inno 2 '120363030052837165@g.us'
+    //MEC '120363039655415935@g.us'
+    //AHI '6285693335165-1525781294@g.us'
+    //Webinar '120363277409587491@g.us'
+    //RME '120363186013542533@g.us'
+    //Startup Nusantara '120363115791085529@g.us'
+    //Collab Uang '120363048188666997@g.us'
+    //St Frans '6282134349123-1494248370@g.us'
+    //NW '120363038707327926@g.us'
+    //Pertanian Teknologi '120363163875111753@g.us'
+    //Early MD '120363048054248610@g.us'
+    
     sock.ev.on('creds.update', saveCreds);
 
     sock.ev.on('messages.upsert', async ({ messages, type }) => {
